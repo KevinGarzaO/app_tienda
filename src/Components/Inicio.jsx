104597bc69ddf60fbade2e334680d49b2303676f
Kevin Garza <kevin_4569@msn.com>
2022-01-15 12:02:59 -0600

    Agregando paginas

---
+++ src/Components/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inicio = () => {
+    return ( 
+        <div>
+            <h1>Inicio</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut rem molestiae ducimus sint magni nam doloribus dicta, repudiandae mollitia quam similique saepe libero magnam natus perferendis sit porro beatae fuga?</p>
+        </div>
+     );
+}
+ 
+export default Inicio;
---
+++ src/Components/Tienda.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Tienda = () => {
+    return ( 
+        <div>
+            <h1>Tienda</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut rem molestiae ducimus sint magni nam doloribus dicta, repudiandae mollitia quam similique saepe libero magnam natus perferendis sit porro beatae fuga?</p>
+        </div>
+     );
+}
+ 
+export default Tienda;
