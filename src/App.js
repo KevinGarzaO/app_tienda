c98798c55cf11c6f99b8277b7dc156aecad3f93e
Kevin Garza <kevin_4569@msn.com>
2022-01-19 11:46:06 -0600

    Agregamos articulos al carrito

--- src/App.js
+++ src/App.js
@@ -17,6 +17,54 @@ const App = () => {
 
 const [carrito, setCarrito] = useState([]);
 
+const agregarProductoAlCarrito = (idProductoAgregar, nombre) =>{
+    
+    //Si el carrito no tiene elementos entonces agregamos uno.
+    if(carrito.length === 0){
+        setCarrito([{id: idProductoAgregar, nombre: nombre, cantidad: 1}]);
+    } else{
+      //De otra forma tenemos que revisar que el carrito no tenga ya el producto que queremos agregar.
+      //Si ya lo tiene entonces queremos actualizar a su valor.
+      //si no tiene el producto entonces lo agregamos.
+     
+      //Para editar el arreglo tenemos que clonarlo.
+      const nuevoCarrito = [...carrito];
+
+      //Comprobamos si el carrito ya tiene el ID del producto a agregar.
+      const yaEstaEnCarrito =   nuevoCarrito.filter((productoDeCarrito) => {
+            return productoDeCarrito.id === idProductoAgregar 
+        }).length > 0;
+
+        //Si ya tiene el producto entonces lo tenemos que actualizar.
+        if(yaEstaEnCarrito){
+            //Para ello tenemos que buscarlo, obtener su posición en el arreglo.
+            //y en base a su posición ya actualizamos el valor.
+            nuevoCarrito.forEach((productoDeCarrito, index) =>{
+                if(productoDeCarrito.id === idProductoAgregar){
+                    const cantidad = nuevoCarrito[index].cantidad
+                    nuevoCarrito[index] = 
+                    {
+                      id: idProductoAgregar, 
+                      nombre: nombre, 
+                      cantidad: cantidad + 1
+                    }
+                }
+            })
+            //De otra forma entonces agregamos el producto al arreglo
+        }else{
+          nuevoCarrito.push({
+            id: idProductoAgregar,
+            nombre: nombre,
+            cantidad: 1
+          });
+        }
+
+        //Por ultimo actualizamos el carrito.
+        setCarrito(nuevoCarrito);
+      
+    }
+}
+
   return ( 
      
      <Contenedor>
@@ -31,7 +79,10 @@ const [carrito, setCarrito] = useState([]);
                   <Route path="*" element={<Error404 />} />
                   <Route path="/" element={<Inicio />} />
                   <Route path="/blog" element={<Blog />} />
-                  <Route path="/tienda" element={ <Tienda  productos={productos}/>} />
+                  <Route path="/tienda" element={ <Tienda  
+                                                    productos={productos} 
+                                                    agregarProductoAlCarrito = {agregarProductoAlCarrito}
+                                                    />} />
               </Routes>
         </main>
         <aside>
